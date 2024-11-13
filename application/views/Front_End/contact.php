ce043a83fe82762a3b07d972774822015335227b
Muhammad-Daniyal0077 <muhammaddaniyal00777@gmail.com>
2024-11-13 17:10:19 +0500

    many changes

--- application/views/Front_End/contact.php
+++ application/views/Front_End/contact.php
@@ -18,10 +18,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
 <style>
-    h1{
-        margin: 40px;
-
-    }
     /* Custom styling for the contact info box */
 .contact-info-box {
     background-color: #fff;
