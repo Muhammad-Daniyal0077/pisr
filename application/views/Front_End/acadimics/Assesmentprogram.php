fd2d60883c649de6868942014d44d26ddde0432d
Muhammad Shaban Saleem <muhammadshabansaleem940@gmail.com>
2024-12-04 11:08:53 +0500

    updated

--- application/views/Front_End/acadimics/Assesmentprogram.php
+++ application/views/Front_End/acadimics/Assesmentprogram.php
@@ -1,8 +1,7 @@
 <!-- Page Title -->
 <div class="pic">
 	<div class="page-title dark-background" data-aos="fade"
-		style="background-image: url(https://pisr.org/v2/wp-content/uploads/2021/06/Untitled-design1.png);">
-		<div class="background-overlay"></div> <!-- Overlay div for dull effect -->
+	style="background-image: url(<?php echo base_url(); ?>asset/images/downloads/background.png);">		<div class="background-overlay"></div> <!-- Overlay div for dull effect -->
 		<div class="container">
 			<h1 class="animated-title">Assessment Programs</h1>
 			<nav class="breadcrumbs">
@@ -14,3 +13,38 @@
 		</div>
 	</div>
 </div>
+
+
+
+<section>
+  <div class="container">
+    <div class="head">
+      <h3>Assessment Policy</h3>
+    </div>
+    <p class="para">Our school maintains a structured academic calendar following the two-semester program, emphasizing Continuous Assessment through two formative assessments per semester. These assessments play a significant role in determining the final term report card, providing a comprehensive evaluation of student progress and understanding.</p>
+
+    <div class="head">
+      <h4>Comprehensive Review Material:</h4>
+    </div>
+    <p class="para">
+      To ensure thorough understanding and retention of material, students are provided with Review Sheets and pop-up quizzes after completing each chapter or unit. This approach not only reinforces learning but also prepares students for the upcoming assessments.
+    </p>
+
+    <div class="head">
+      <h4>Diagnostic Tests:</h4>
+    </div>
+    <p class="para">
+      Prior to diving into a new chapter or unit, our dedicated teachers administer diagnostic tests to identify any potential learning gaps among students. This proactive approach allows educators to tailor their teaching methods to address individual needs and ensure comprehensive understanding.
+    </p>
+
+    <div class="head">
+      <h4>Assessment for Learning (AFL):</h4>
+    </div>
+    <p class="para">In order to monitor student progress on a regular basis, our teachers employ Assessment for Learning strategies at the end of each lesson. This allows educators to gauge the level of achievement of lesson objectives and make necessary adjustments to enhance student learning.</p>
+
+    <div class="head">
+      <h4>Ipsative tests:</h4>
+    </div>
+    <p class="para">Additionally, our teachers conduct ipsative tests on a monthly basis to track and evaluate students' academic progress, providing valuable feedback and support for continuous improvement. This commitment to ongoing assessment and feedback ensures that each student has the opportunity to reach their full potential academically.</p>
+  </div>
+</section>
